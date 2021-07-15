2ddcd16720a8f0b846ba36fdfe7646c1f1014bb5
Filip Sobol <sobol.filip@gmail.com>
2021-07-15 11:52:25 +0200

    Update tests

--- tests/core/watchSources.spec.ts
+++ tests/core/watchSources.spec.ts
@@ -1,7 +1,11 @@
 import watchSources from '../../src/core/watchSources';
 import * as chokidar from 'chokidar';
 
-jest.mock('chokidar');
+jest.mock('chokidar', () => ({
+  watch: jest.fn().mockImplementation(() => ({
+    on: jest.fn()
+  }))
+}));
 
 const config: any = {
   copy: {
@@ -16,17 +20,13 @@ const config: any = {
 
 describe('watchSources', () => {
   it('calls watch library with proper paths', () => {
-
-    jest.spyOn(chokidar, 'watch').mockImplementationOnce((paths, options) => {
-      expect(paths).toEqual([
-        `${config.copy.from[0].path}/**/*`,
-        `${config.copy.from[1].path}/**/*`,
-        `${config.copy.from[2].path}/**/*`
-      ]);
-
-      return { on: () => {} } as any;
-    });
+    const watchSpy = jest.spyOn(chokidar, 'watch');
+    const anyConfig = expect.objectContaining({});
 
     watchSources({ config, sourcesMap: new Map() });
+
+    expect(watchSpy).toHaveBeenNthCalledWith(1, `${config.copy.from[0].path}/**/*`, anyConfig);
+    expect(watchSpy).toHaveBeenNthCalledWith(2, `${config.copy.from[1].path}/**/*`, anyConfig);
+    expect(watchSpy).toHaveBeenNthCalledWith(3, `${config.copy.from[2].path}/**/*`, anyConfig);
   });
 });
